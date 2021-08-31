fbb602b03c08fa4dc4f6794d290334d320280edb
Tharuka Kalhara <33922390+TheFoxDecoder@users.noreply.github.com>
2021-08-31 10:55:03 +0530

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main(){
-		std::cout<<"Hey there! Greating from main function"<<std::endl;
+		std::cout<<"Hey there! Greeting from main function"<<std::endl;
 		return 0;
 }
