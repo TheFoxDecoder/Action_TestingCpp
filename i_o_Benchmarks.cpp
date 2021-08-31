936d4a9eca87ea44459f7f68536fccd2f0e61cca
Tharuka Kalhara <33922390+TheFoxDecoder@users.noreply.github.com>
2021-09-01 02:57:27 +0530

    Workflow (#2)
    * git i_o_Benchmarks change to header to running file (hpp --> cpp)
    
    * get the same typlical run greeting msg
    
    * fix the cmake also
    
    * compile command programs
    
    * del file add
    
    * added testing program
    
    * added lines
    
    * workflow action of github

--- i_o_Benchmarks.cpp
+++ i_o_Benchmarks.cpp
@@ -134,3 +134,36 @@ void testPosixIO(const char* inFile, const char* outFile, std::vector<char>& inB
     ::close(out);
     ::close(in);
 }
+
+int benchmarks(int argc, char* argv[]){
+    std::vector<std::string> args(argv, argv + argc);
+    if (args.size() != 4)
+    {
+        std::cout << "Usage: " << args[0] << " copy_method (c, posix, c++) in_file number_of_times" << std::endl;
+        return 1;
+    }
+
+    typedef std::map<std::string, std::function<void (const char*, const char*, std::vector<char>&)>> FuncMap;
+    FuncMap funcMap { {"c", testCFileIO}, {"posix", testPosixIO}, {"c++", testCppIO}};
+
+    auto it = funcMap.find(args[1]);
+    if (it != funcMap.end())
+    {       
+        std::vector<char> inBuffer(1024 * 1024);
+        
+        auto dest = args[2] + ".copy";
+        const auto times = std::stoul(args[3]);
+        
+        milliseconds::rep total = 0;
+        for (unsigned int i = 0; i < times; ++i)
+        {
+            total += measure::ms(it->second, args[2].c_str(), dest.c_str(), inBuffer);
+            ::unlink(dest.c_str());
+        }
+        std::cout << "Average " << args[1] << " I/O took: " << total / double(times) << "ms" << std::endl;
+    }
+    else
+    {
+        std::cout << "Not supported copy method: " << args[1] << std::endl;
+    }    
+}
--- main.cpp
+++ main.cpp
@@ -1,42 +1,6 @@
 #include <iostream>
-#include "i_o_Benchmarks.hpp"
 
-<<<<<<< HEAD
 int main(){
-		std::cout<<"Hey there! Greeting from main function"<<std::endl;
-		return 0;
+	std::cout<<"Hey there! Greeting from main function"<<std::endl;
+	return 0;
 }
-=======
-int main(int argc, char* argv[]){
-    std::vector<std::string> args(argv, argv + argc);
-    if (args.size() != 4)
-    {
-        std::cout << "Usage: " << args[0] << " copy_method (c, posix, c++) in_file number_of_times" << std::endl;
-        return 1;
-    }
-
-    typedef std::map<std::string, std::function<void (const char*, const char*, std::vector<char>&)>> FuncMap;
-    FuncMap funcMap { {"c", testCFileIO}, {"posix", testPosixIO}, {"c++", testCppIO}};
-
-    auto it = funcMap.find(args[1]);
-    if (it != funcMap.end())
-    {       
-        std::vector<char> inBuffer(1024 * 1024);
-        
-        auto dest = args[2] + ".copy";
-        const auto times = std::stoul(args[3]);
-        
-        milliseconds::rep total = 0;
-        for (unsigned int i = 0; i < times; ++i)
-        {
-            total += measure::ms(it->second, args[2].c_str(), dest.c_str(), inBuffer);
-            ::unlink(dest.c_str());
-        }
-        std::cout << "Average " << args[1] << " I/O took: " << total / double(times) << "ms" << std::endl;
-    }
-    else
-    {
-        std::cout << "Not supported copy method: " << args[1] << std::endl;
-    }    
-}
->>>>>>> 95874fb4ae83b81fafd48fea1ad298165779ab95
