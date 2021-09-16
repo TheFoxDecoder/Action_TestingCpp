543c809b1de4e70cde5b269ff8a55e274b9d7cf9
TheFoxDecoder <tharuka.kalhara98917@gmail.com>
2021-09-17 03:41:37 +0530

    update

--- main.cpp
+++ main.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 
 void fibbancchi(int a){
-	int tot,as;
 	for(int i=0;i<a;++i){
 		
-		std::cout<<(i-1)-(i-2)<<", ";
+		std::cout<<(i-1)+(i-2)<<", ";
 	}
 }
 
 int main(){
 	int num;
-	std::cout<<"Enter number: "<<std::endl;
+	std::cout<<"Enter number: ";
 	std::cin>>num;
 	
 	// fail safe
-	if(std::cin.fail() == NULL){
+	if(std::cin.fail()){
 		num=100;
 	}
 	
